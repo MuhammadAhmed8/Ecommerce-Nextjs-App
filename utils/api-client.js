88266b44045860aa82a59b9ad325b2b904e7a9a3
MuhammadAhmed8 <muhammad1999ma@gmail.com>
2021-09-26 19:36:09 +0500

    base url changed

--- utils/api-client.js
+++ utils/api-client.js
@@ -2,8 +2,8 @@ import CezerinClient from 'cezerin-client';
 
 const apiClient = (token="<token>") => {
     return new CezerinClient({
-        apiBaseUrl: 'http://localhost:3001/api/v1',
-        ajaxBaseUrl: 'http://localhost:3001/ajax',
+        apiBaseUrl: 'http://165.22.68.190/api/v1',
+        ajaxBaseUrl: 'http://165.22.68.190/ajax',
         apiToken: token
     });
 }
--- utils/config.js
+++ utils/config.js
@@ -1,5 +1,5 @@
-const apiBase="http://localhost:3001/api/v1/";
-const ajaxBase = "http://localhost:3001/ajax/";
+const apiBase="http://165.22.68.190/api/v1/";
+const ajaxBase = "http://165.22.68.190/ajax/";
 
 const config = {
     apiBase,
